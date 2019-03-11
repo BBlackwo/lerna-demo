26ff817a362d57fb8d1381a03f9c46de77af9aa4
Benjamin Blackwood <ben.blackwood@auspost.com.au>
2019-03-12 10:39:36 +1100

    add hello-world-2 and symlink

--- packages/hello-world-1/lib/hello-world-1.js
+++ packages/hello-world-1/lib/hello-world-1.js
@@ -7,5 +7,3 @@ module.exports = helloWorld1;
 function helloWorld1() {
   console.log(chalk.green('Hello') + ' World ' + chalk.red('1'));
 }
-
-helloWorld1();
---
+++ packages/hello-world-2/README.md
@@ -0,0 +1,11 @@
+# `hello-world-2`
+
+> TODO: description
+
+## Usage
+
+```
+const helloWorld2 = require('hello-world-2');
+
+// TODO: DEMONSTRATE API
+```
